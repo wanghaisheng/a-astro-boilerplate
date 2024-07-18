58d1afcba74ed2a1698c3c5ecefcf408513b176d
tatumertanen <144846801+tatumertanen@users.noreply.github.com>
2024-07-18 22:29:28 +0300

    Remove NavLinks type and config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,25 +1,6 @@
-import type { Site, NavLinks } from '@/types'
+import type { Site } from '@/types'
 
 export const site: Site = {
     name: "Astro Boilerplate",
     description: "Astro Boilerplate with basic integrations including TailwindCSS",
 }
-
-export const navlinks: NavLinks = [
-    {
-        name: "Home",
-        path: "/",
-    },
-    {
-        name: "About",
-        path: "/about",
-    },
-    {
-        name: "Services",
-        path: "/services",
-    },
-    {
-        name: "Contact",
-        path: "/contact",
-    },
-]
--- src/types.ts
+++ src/types.ts
@@ -2,10 +2,3 @@ export type Site = {
     name: string
     description: string
 }
-
-export type NavLink = {
-    name: string
-    path: string
-}
-
-export type NavLinks = NavLink[]
