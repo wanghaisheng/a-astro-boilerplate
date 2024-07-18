e6fb95ad1abe50c3dd287c465c7e655b3d8ec7ec
tatumertanen <144846801+tatumertanen@users.noreply.github.com>
2024-07-18 21:16:43 +0300

    Add custom spacing sizes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,12 @@ export default {
 	darkMode: 'media',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			spacing: {
+				'8xl': '96rem',
+				'9xl': '128rem',
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
