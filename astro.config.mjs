afdce7f8a246f74db4b3be1dab6840be8c8fd662
tatumertanen <144846801+tatumertanen@users.noreply.github.com>
2024-07-18 20:41:03 +0300

    Install Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://your-site.com',
+  site: 'https://your-site.com',
+  integrations: [tailwind()]
 });
