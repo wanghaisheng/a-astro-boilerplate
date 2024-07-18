9a09e2b9f04abe35cd8cb39f48055a0787f1f2e6
tatumertanen <144846801+tatumertanen@users.noreply.github.com>
2024-07-18 21:22:33 +0300

    Install astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import partytown from "@astrojs/partytown";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://your-site.com',
-  integrations: [tailwind(), sitemap(), partytown()]
+  integrations: [tailwind(), sitemap(), partytown(), icon()]
 });
