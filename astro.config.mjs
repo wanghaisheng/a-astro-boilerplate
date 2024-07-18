893ec85d52261d23f498b1d7d53e1c5f3f6e7c44
tatumertanen <144846801+tatumertanen@users.noreply.github.com>
2024-07-18 20:45:58 +0300

    Install & initialize Sitemap
    Placeholder site put in public/robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://your-site.com',
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()]
 });
