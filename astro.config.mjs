b20227d1a75689c045ee8b0fdc4df5524738421c
tatumertanen <144846801+tatumertanen@users.noreply.github.com>
2024-07-18 20:47:41 +0300

    Install Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://your-site.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), partytown()]
 });
